2fa8762c07cd06670bb2a09a0e0a9a00aaf2d2bd
Heath Hamilton <34899369+hahamilton@users.noreply.github.com>
2020-04-22 02:19:41 -0500

    Removed extraneous comments.

--- dev_tools/find_site_urls.py
+++ dev_tools/find_site_urls.py
@@ -3,11 +3,6 @@ import re
 
 tree = sitemap_tree_for_homepage('https://documents.polycom.com/')
 
-# pdl_links = []
-
-# with open('urls.csv', 'w', newline='') as f:
-#	thewriter = csv.writer(f)
-
 for page in tree.all_pages():
 	data = re.findall("(https:\/\/.+)", page)
 	for url in data:
